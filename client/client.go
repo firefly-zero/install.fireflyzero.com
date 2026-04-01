982e8e395cb0e0ad08579b8bc6e5d87da32c52c7
gram <git@orsinium.dev>
2026-04-01 19:34:53 +0200

    http-based demo client

--- client/client.go
+++ client/client.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"log"
 	"math/rand/v2"
-	"net"
+	"net/http"
 )
 
 func main() {
@@ -17,51 +17,49 @@ func main() {
 }
 
 func run() error {
-	conn, err := net.Dial("tcp", "127.0.0.1:19743")
-	if err != nil {
-		return fmt.Errorf("dial: %v", err)
-	}
-	id := rand.Uint32() % 100_000_000
-	fmt.Printf("session ID: %8d\n", id)
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, id)
-	_, _ = conn.Write(buf)
+	idUint := rand.Uint32() % 100_000_000
+	idStr := fmt.Sprintf("%8d", idUint)
+	fmt.Println("session ID:", idStr)
 
+	url := "http://127.0.0.1:19742/download/" + idStr
 	fmt.Println("waiting for ROM...")
-	rom, err := io.ReadAll(conn)
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("send request: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %v", resp.Status)
+	}
+	fmt.Println("downloading ROM...")
+
+	defer resp.Body.Close()
+	rom, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return fmt.Errorf("download ROM: %v", err)
 	}
-	printROM(rom)
+	printROM(resp.Header, rom)
 	return nil
 }
 
-func printROM(rom []byte) {
-	fmt.Printf("received:         %d bytes\n", len(rom))
-	fmt.Printf("protocol version: %d\n", rom[0])
-	fmt.Printf("body size:    	  %d bytes\n", binary.LittleEndian.Uint32(rom[1:]))
-	rom = rom[5:]
+func printROM(headers http.Header, body []byte) {
+	fmt.Println()
+	fmt.Printf("received:         %d bytes\n", len(body))
+	fmt.Printf("protocol version: %s\n", headers.Get("X-F0-Protocol"))
+	fmt.Printf("body size:    	  %s bytes\n", headers.Get("X-F0-Size"))
+	fmt.Printf("author ID:        %s\n", headers.Get("X-F0-Author"))
+	fmt.Printf("app ID:           %s\n", headers.Get("X-F0-App"))
+	fmt.Println()
 
-	size := binary.LittleEndian.Uint32(rom)
-	rom = rom[4:]
-	fmt.Printf("author ID:        %s\n", string(rom[:size]))
-	rom = rom[size:]
+	for len(body) != 0 {
+		size := binary.LittleEndian.Uint32(body)
+		body = body[4:]
+		name := string(body[:size])
+		body = body[size:]
 
-	size = binary.LittleEndian.Uint32(rom)
-	rom = rom[4:]
-	fmt.Printf("app ID:           %s\n", string(rom[:size]))
-	rom = rom[size:]
+		size = binary.LittleEndian.Uint32(body)
+		body = body[4:]
+		body = body[size:]
 
-	for len(rom) != 0 {
-		size = binary.LittleEndian.Uint32(rom)
-		rom = rom[4:]
-		name := string(rom[:size])
-		rom = rom[size:]
-
-		size = binary.LittleEndian.Uint32(rom)
-		rom = rom[4:]
-		rom = rom[size:]
-
-		fmt.Printf("%s: %d bytes\n", name, size)
+		fmt.Printf("%-16s %6d bytes\n", name, size)
 	}
 }
